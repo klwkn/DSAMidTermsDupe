b67eb33f21642b45ed82bb5ed801ad0bb0a47656
KLWKN <rjoaquin.buenaflor@gmail.com>
2023-11-29 19:56:38 +0800

    I ctrl+z'd back to the past.

--- DSAMidTermsDupe/Program.cs
+++ DSAMidTermsDupe/Program.cs
@@ -43,6 +43,7 @@ namespace DSAMidTermsDupe
         static void Main(string[] args)
         {
             List<string> list1 = new List<string>();
+            List<List<string>> list = new List<List<string>>();
             Stack<string> customer1 = new Stack<string>();
             Stack<string> customer2 = new Stack<string>();
             Stack<string> customer3 = new Stack<string>();
@@ -82,8 +83,6 @@ namespace DSAMidTermsDupe
             Console.WriteLine("".PadLeft(38, '-'));
 
             int menus = 1;
-            PriorityQueue<string> orders = new PriorityQueue<string>();
-
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine($"\n\nWhich line would you take: Customer {i + 1}");
@@ -96,34 +95,34 @@ namespace DSAMidTermsDupe
                 for (int j = 0; j < quantity; j++)
                 {
                     Console.Write("\nChoose the number of the food item you want from the menu: ");
-                    int foodnum = Convert.ToInt32(Console.ReadLine());
+                    int foodnum = Convert.ToInt32((Console.ReadLine()));
+
                     Console.Write("\nInsert Quantity of order: ");
                     int foodquan = Convert.ToInt32(Console.ReadLine());
-
                     while (menus < list1.Count)
                     {
-                        if (foodnum == Convert.ToInt32(list1[menus]))
+                        if (Convert.ToString(foodnum) == (list1[menus]))
                         {
-                            subtotal += foodquan * Convert.ToInt32(list1[menus + 2]);
-                            orders.EnqueueSort(list1[menus + 1], foodnum, subtotal.ToString());
+                            subtotal += foodquan * Convert.ToInt32(list1[menus+2]);
+                            if (i == 0)
+                            {
+                                customer1.Push(list1[menus+1]);
+                            }
+                            else if (i == 1)
+                            {
+                                customer2.Push(list1[menus+1]);
+                            }
+                            else
+                            {
+                                customer3.Push(list1[menus+1]);
+                            }
                         }
-                        menus += 3;
+                        menus+= 3;
                     }
                     menus = 1;
                 }
-
                 string custnum = (i + 1).ToString().PadLeft(4, '0');
                 Console.WriteLine($"Customer {i + 1} ticket number: {custnum}");
-
-            }
-
-            Console.WriteLine("\nList of Orders:");
-            Console.WriteLine("".PadLeft(38, '-'));
-            while (orders.Count > 0)
-            {
-                var order = orders.Dequeue();
-                Console.WriteLine($"| {order.item} |");
-                Console.WriteLine("".PadLeft(38, '-'));
             }
 
             Console.ReadKey();
